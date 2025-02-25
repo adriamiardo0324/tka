62d04620bcab555405c0d045694a6016eeef64d0
Tanjona <andrianiainatanjona67@gmail.com>
2025-02-25 11:17:02 +0300

    environnement ubuntu

--- user.py
+++ user.py
@@ -1,21 +1,38 @@
-from tkinter import *
 import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
 
-root = tk.Tk()
+root=tk.Tk()
+root.geometry("500x400")
+root.title("MANIA HOTEL")
 
-root.title("Utulisateur")
-root.geometry("400x300")
 
-label1 = tk.Label(root,text="username:")
-label1.place(x=20,y=50)
-input1 = tk.Entry(root)
-input1.place(x=20,y=80)
+def se_connecter():
+    utilisateur = username.get()
+    mot_de_passe = password.get()
 
-label2 = tk.Label(root,text="Mot de passe:")
-label2.place(x=20,y=110)
-input2 = tk.Entry(root)
-input2.place(x=20,y=130)
-btn = tk.Button(root,text="Connexion",bg="#333",padx=60)
-btn.place(x=20,y=170)
+image = Image.open("/home/tanjona/Documents/mania_hotel/logo.png")
+image_tk = ImageTk.PhotoImage(image)
+label_image = tk.Label(root,image=image_tk)
+label_image.pack()
+
+frame = tk.Frame(root)
+frame.pack(expand=True,pady=5)
+
+tk.Label(frame,text="usename:",font="Arial").pack(padx=10)
+username = tk.Entry(frame,width=30)
+username.pack(padx=10,pady=5)
+
+tk.Label(frame,text="password:",font="Arial").pack(padx=10)
+password = tk.Entry(frame,width=30,show="*")
+password.pack(padx=10,pady=5)
+
+
+connexion = tk.Button(root,text="Se connecter",command=se_connecter,font="Arial",width=24,bg="#2c8feb",fg="white")
+connexion.pack()
+
+
+
+
 root.mainloop()
 
