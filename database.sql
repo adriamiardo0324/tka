8a4ff74a77f606deaea03af70fe417a99a2b8135
Tanjona <andrianiainatanjona67@gmail.com>
2025-03-02 23:21:30 +0300

    modification base de donnée

--- database.sql
+++ database.sql
@@ -1,3 +1,7 @@
+create database if not exists Mania_hotel;
+USE Mania_hotel;
+
+DROP TABLE IF EXISTS 'utilisateurs';
 CREATE TABLE utilisateurs(
     id INT AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(200) NOT NULL,
@@ -6,6 +10,7 @@ CREATE TABLE utilisateurs(
     role ENUM ("gerant","receptioniste")
 );
 
+DROP TABLE IF EXISTS 'clients';
 CREATE TABLE clients(
     id INT AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(200) NOT NULL,
@@ -13,16 +18,21 @@ CREATE TABLE clients(
     telephone INT
 );
 
+DROP TABLE IF EXISTS 'chambres';
 CREATE TABLE chambres(
     id INT AUTO_INCREMENT PRIMARY KEY,
     type VARCHAR(200),
+    numero_chambre INT
 );
 
-CREATE TABLE reservation(
+DROP TABLE IF EXISTS 'reservations';
+CREATE TABLE reservations(
     id INT AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(200) NOT NULL,
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     id_chambre INT,
+    id_client INT,
     reserver VARCHAR(200),
-    FOREIGN KEY id_chambre REFERENCES chambres(id) ON DELETE CASCADE
+    FOREIGN KEY id_chambre REFERENCES chambres(id) ON DELETE CASCADE,
+    FOREIGN KEY id_client REFERENCES clients(id) ON DELETE CASCADE
 );
